b03aa742a766a7967c04639f1512ee78d671b13d
mrsuber <msiysinyuy@gmail.com>
2022-06-04 11:43:19 +0100

    putting a button in react using TouchableOpacity

--- ResturantApp/App.js
+++ ResturantApp/App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native';
 import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
+
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+
         <Text style={styles.instruction}>
               To share a photo from your phone with a friend, just press the button below!
         </Text>
+
+        <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -24,11 +33,23 @@ const styles = StyleSheet.create({
   logo:{
     width: 305,
     height: 159,
-    marginBottom: 10,
+    marginBottom: 20,
+
   },
   instruction:{
     color:"#888",
      fontSize:18,
      marginHorizontal: 15,
+     marginBottom: 10,
    },
+   button:{
+     backgroundColor: 'blue',
+     padding: 20,
+     borderRadius: 5
+   },
+   buttonText:{
+     fontSize: 20,
+     color: '#fff' 
+
+   }
 });
