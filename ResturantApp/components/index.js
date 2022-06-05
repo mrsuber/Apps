d97bbeb52777bae05d410ad012aab405f61bb800
mrsuber <msiysinyuy@gmail.com>
2022-06-05 16:53:25 +0100

    domi data added and the header section completed

--- ResturantApp/components/index.js
+++ ResturantApp/components/index.js
@@ -1,2 +1,3 @@
 export {default as TabBarCustomButton} from './tabBarCustomButton/TabBarCustomButton'
 export {default as CustomTabBar} from './customTabBar/CustomTabBar'
+export {default as Header} from './Header/Header'
--- ResturantApp/screens/Home.js
+++ ResturantApp/screens/Home.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import {View,Text} from 'react-native'
-
+import React,{useState} from 'react';
+import {View,Text,SafeAreaView,StyleSheet,TouchableOpacity,Image,FlatList} from 'react-native'
+import {icons,images,SIZES,COLORS,FONTS} from '../constants'
+import {Header} from '../components'
+import {categoryData,restaurantData,initialCurrentLocation} from '../domiData/domiData'
 const Home = () =>{
+  const [categories, setCategories] = useState(categoryData)
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [restaurants, setRestaurants] = useState(restaurantData)
+  const [currentLocation, setCurrentLocation] = useState(initialCurrentLocation)
+
   return(
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header location={currentLocation}/>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: COLORS.lightGray4
+  },
+  shadow: {
+      shadowColor: "#000",
+      shadowOffset: {
+          width: 0,
+          height: 3,
+      },
+      shadowOpacity: 0.1,
+      shadowRadius: 3,
+      elevation: 1,
+  }
+})
 
 export default Home;
